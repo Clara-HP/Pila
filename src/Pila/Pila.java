c5bccf39bb892c3d974223010ebec921dea84a80
Clara-HP <garciagarciaclar7r@gmail.com>
2019-11-12 18:08:36 -0600

    iii

---
+++ src/Pila/Pila.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pila;
+
+/**
+ *
+ * @author 
+ */
+public class Pila {
+    int [] pila;
+    int cima = 0;
+    int fondo = 0;
+    
+    public Pila(int [] pila){
+        this.pila = pila;
+    }
+    public void insertar(int dato){
+        pila[cima] = dato;
+        cima++;
+    }
+    public void retirar(int dato){
+        pila[cima] = 0;
+        cima++;
+    }
+
+    void mostrarPila() {
+        for (int i = pila.length; i > 0; i--) {
+            if (pila[i-1] !=0) {
+                 System.out.println(pila[i-1] );
+            }
+           
+        }
+        
+    }
+    public int  cima(){
+        System.out.println("La sima de la pila es:" + pila[cima - 1]);
+        return pila[cima - 1];
+    }
+    public int fondo(){
+       
+        System.out.println("El fondo de la pila es: " + pila[0]);
+        return pila[fondo];
+    }
+    public boolean esVacia(){
+        boolean vacia = false;
+        for (int i = 0; i < pila.length; i++) {
+            if (pila[i] == 0) {
+                vacia = true;
+                break;
+            }else{
+                vacia = false;
+            }
+        }
+        return vacia;
+    }
+    public boolean esLlena(){
+        boolean llena = false;
+        if (pila[pila.length - 1] !=  0) {
+            llena = true;        
+        } else{
+            llena = false;
+        }
+        return llena;
+    }
+    
+    public int tamaño(){
+         return cima;
+     }
+    public void LimpiarPila(){
+        for (int i = 0; i < pila.length; i++) {
+            pila[i] = 0;
+        }
+    }
+
+    void retirar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
